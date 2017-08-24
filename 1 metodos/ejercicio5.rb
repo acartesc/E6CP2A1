0135b871e4ccd339477ea760bc2c4def985e8933
Andres <acartescc@gmail.com>
2017-08-23 21:22:41 -0300

    1 metodos - ejercicio5 rev1 y 6 terminado

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -2,6 +2,7 @@
 # e imprima los números pares que existen entre esos dos números.
 def comparison(num1, num2)
   (num1..num2).each { |i| puts i if i.even? }
+  (num2..num1).each { |i| puts i if i.even? }
 end
 
-comparison(3, 12)
+comparison(12, 3)
