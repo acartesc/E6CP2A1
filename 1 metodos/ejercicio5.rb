1167269f533a7c45b86dcbf6e9999f65ded4dad8
Andres <acartescc@gmail.com>
2017-08-23 21:02:35 -0300

    1 metodos - ejercicio5 terminado

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,7 @@
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+def comparison(num1, num2)
+  (num1..num2).each { |i| puts i if i.even? }
+end
+
+comparison(3, 12)
