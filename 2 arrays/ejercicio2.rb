8ca430c97f9101b69af4a505c5d0a9defdbd87d9
Andres <acartescc@gmail.com>
2017-08-27 19:25:06 -0300

    2 arrays - ejercicio 2 finalizado

--- 2 arrays/ejercicio2.rb
+++ 2 arrays/ejercicio2.rb
@@ -10,3 +10,26 @@
 #    invertir el orden de los elementos en un arreglo.
 
 a = [1,2,3,9,1,4,5,2,3,6,6]
+
+a.pop
+puts a
+
+a.delete_at(0)
+puts a
+
+
+if a.length.odd? 
+ 	a.delete_at(a.length/2)
+ 	puts a
+else
+	a.delete_at(a.length/2 - 1) 	
+	puts a
+end 
+
+a.pop if a[-1] != 1
+
+b = []
+
+a.each { b.push(a.pop) }
+
+puts b
