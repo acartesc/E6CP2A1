a2a7446278bbb38708455461eae8aa3a4423c9e8
Andres <acartescc@gmail.com>
2017-08-30 19:51:51 -0300

    2 arrays - ejercicio 5 finalizado

--- 2 arrays/ejercicio5.rb
+++ 2 arrays/ejercicio5.rb
@@ -7,9 +7,13 @@
 products = %w(Producto1 Producto2 Producto3 Producto4)
 prices = %w[1000 2000 1500 950]
 
+counter = 0
 html = ''
-products.each do |i|
+products.each_with_index do |i|
   html += "<div class='product'>"
+  html += "<p> #{i} </p>"
+  html += "<p> #{prices[counter]} </p>"
+  counter += 1
   html += "</div>\n"
 end
 
