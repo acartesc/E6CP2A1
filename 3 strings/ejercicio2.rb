c48dd8166201cf1834bc9c78ae3b210339c892e8
Andres <acartescc@gmail.com>
2017-09-01 17:08:01 -0300

    3 strings - ejercicio 2 finalizado

--- 3 strings/ejercicio2.rb
+++ 3 strings/ejercicio2.rb
@@ -1,4 +1,20 @@
 # Construir un arreglo de los nombres de todos sus compañeros y en base a él:
+
+curso = %w(Cristian Joselyne Richard Miguel Manoli Camila Fernanda Javier Andrés)
+
 # 1. Imprimir todos los elementos que excedan más de 5 caracteres.
+curso.each { |e| print "#{e} " if e.length >5}
+puts "\n ########################## \n"
+
 # 2. Crear un arreglo nuevo con todos los elementos en minúscula.
+curso2 = curso.map { |e| e.downcase }
+print curso2
+puts "\n ########################## \n"
+
 # 3. Crear un método que devuelva un arreglo con la cantidad de caracteres que tiene cada nombre.
+def name_length(array)
+  array = array.map {|e| e.length }
+  print "#{array} \n"
+end
+
+name_length(curso)
